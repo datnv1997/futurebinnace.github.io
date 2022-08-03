c25492ded436706a07ce2050a6b78f0ca38ff8e2
Nguyen Dat <datnv1997@gmail.com>
2022-08-04 00:46:23 +0700

    Add column sideway

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const allSymbols = [];
 const indexLastestPriceOrClose = 4;
 const indexVol = 5;
 const indexOldPrice = 1 // price open time
+const indexLowestPrice = 3;
+const indexHighestPrice = 2;
+const setting = {
+  sidewayPrice: 3,
+  volatilityVol: 10
+}
+
 
 function main() {
   fetch('https://fapi.binance.com/fapi/v1/ticker/price')
@@ -13,27 +20,28 @@ function main() {
           allSymbols.push(item.symbol)
         })
 
-      const combineData15m = [];
+      const combineData = [];
       Promise.all(allSymbols.map(async symbol => {
         const resp = await getDataCoin(symbol, localStorage.getItem("futureTime") || "1m")
         return resp.json();
       })).then(allResult => {
         allResult.forEach((dataDetailCoin, index) => {
           let currentCandleStick = dataDetailCoin.reverse()[0];
-          const dataHighestPrice = getDataHighestPrice(dataDetailCoin);
-          combineData15m.push({
+          const dataHighestVol = getDataHighestVol(dataDetailCoin);
+          combineData.push({
             coinName: allSymbols[index],
             oldPrice: parseFloat(currentCandleStick[indexOldPrice]),
             lastetPrice: parseFloat(currentCandleStick[indexLastestPriceOrClose]),
             percentChange: `${calculateChangePercentPrice(parseFloat(currentCandleStick[indexLastestPriceOrClose]), parseFloat(currentCandleStick[indexOldPrice]))}%`,
             isVolatilityVol: checkVolatilityVol(dataDetailCoin),
-            timeVolatilityVol: checkVolatilityVol(dataDetailCoin) ? moment(dataHighestPrice[0]).format("HH:mm:ss") : "",
-            timeVolatilityVolFromNow: checkVolatilityVol(dataDetailCoin) ? moment(dataHighestPrice[0]).fromNow() : ""
+            timeVolatilityVol: checkVolatilityVol(dataDetailCoin) ? moment(dataHighestVol[0]).format("HH:mm:ss") : "",
+            timeVolatilityVolFromNow: checkVolatilityVol(dataDetailCoin) ? moment(dataHighestVol[0]).fromNow() : "",
+            sideway: checkSideway(dataDetailCoin)
           })
         })
 
         $(".render-item").empty();
-        combineData15m.forEach(item => {
+        combineData.forEach(item => {
           $(".render-item").append(bindingData(item))
         })
       }
@@ -70,6 +78,7 @@ function bindingData(data) {
     <td style="${isVolatilityVol ? 'color: green; font-weight: bold': ''}">${isVolatilityVol ? "Có" : "Không"}</td>
     <td>${data.timeVolatilityVol}</td>
     <td>${data.timeVolatilityVolFromNow}</td>
+    <td style="${data.sideway ? 'color: green; font-weight: bold': ''}">${data.sideway? "Có" : "Không"}</td>
   </tr>
   `
 }
@@ -78,6 +87,12 @@ function bindingData(data) {
 async function getDataCoin(symbol, interval) {
   return fetch(`https://fapi.binance.com/fapi/v1/klines?symbol=${symbol}&interval=${interval}&limit=15`)
 }
+// getDataCoin("MKRUSDT", "1h").then((response) => response.json())
+// .then((data) => {
+//   const dataFormat = data.reverse()
+//   console.log("data", dataFormat);
+//   checkSideway(dataFormat)
+// })
 
 function checkVolatilityVol(dataCoin) {
   let data = dataCoin.reverse();
@@ -87,13 +102,42 @@ function checkVolatilityVol(dataCoin) {
   let isVolatility = false;
 
   if (currentVol < highestVol) {
-    lowestVol = data.map(item => parseFloat(item[5])).sort(function (a, b) { return a - b })[1];
+    lowestVol = data.map(item => parseFloat(item[indexVol])).sort(function (a, b) { return a - b })[1];
   }
-  if (highestVol > lowestVol * 10) isVolatility = true
+  if (highestVol > lowestVol * setting.volatilityVol) isVolatility = true
 
   return isVolatility
 }
 
-function getDataHighestPrice(dataCoin) {
+function getDataHighestVol(dataCoin) {
   return dataCoin.sort(function (a, b) { return parseFloat(b[indexVol]) - parseFloat(a[indexVol]) })[0];
 }
+function getDataHighestPrice(dataCoin) {
+  return dataCoin.sort(function (a, b) { return parseFloat(b[indexHighestPrice]) - parseFloat(a[indexHighestPrice]) })[0];
+}
+function getDataLowestPrice(dataCoin) {
+  return dataCoin.sort(function (a, b) { return parseFloat(a[indexLowestPrice]) - parseFloat(b[indexLowestPrice]) })[0];
+}
+function checkSideway(dataCoin) { 
+  const highestData = getDataHighestPrice(dataCoin);
+  const lowestData = getDataLowestPrice(dataCoin);
+  console.log("highestData", highestData);
+  console.log("lowestData", lowestData);
+
+  const hightOfLowestCandle = Math.abs(parseFloat(lowestData[indexLastestPriceOrClose]) - parseFloat(lowestData[indexOldPrice]))
+  const hightOfHighestCandle = Math.abs(parseFloat(highestData[indexLastestPriceOrClose]) - parseFloat(lowestData[indexLastestPriceOrClose]))
+  // let flag = dataCoin.some(item => hightOfLowestCandle > 2 * Math.abs(parseFloat(item[indexLastestPriceOrClose]) - parseFloat(item[indexOldPrice])))
+
+  console.log("hightOflowestCandle", hightOfLowestCandle);
+  console.log("hightOfHighestCandle", hightOfHighestCandle);
+
+  const maxHeightAllow = hightOfLowestCandle * setting.sidewayPrice;
+
+  return hightOfHighestCandle < maxHeightAllow;
+
+
+  // let maxPriceAllow = averagePrice * setting.sidewayPrice;
+  // return dataCoin.every(item => parseFloat(item[indexLastestPriceOrClose]) < maxPriceAllow)
+}
+
+
